ff5914e280b6c965e3f301f3c6d05c2e526bc505
Prawal Gangwar <prawal.gangwar@gmail.com>
2023-06-08 16:54:40 +0530

    navigate to relvant page on login

--- src/pages/Login/Login.container.tsx
+++ src/pages/Login/Login.container.tsx
@@ -5,8 +5,11 @@ import { IEmailAndPassword } from './Login.types';
 import Login from './Login.component';
 import { LoginSuccess_Out, OpenAPI } from 'src/client';
 import { DefaultService, Body_login } from 'src/client';
+import { UserRole } from 'src/client';
+import { useNavigate } from 'react-router-dom';
 
 const LoginContainer: React.FC = () => {
+  const navigate = useNavigate();
   async function post(data: IEmailAndPassword): Promise<LoginSuccess_Out> {
     // OpenAPI.BASE = 'https://172.20.100.8:8000';
     if (!process.env.REACT_APP_SAIL_API_SERVICE_URL)
@@ -20,6 +23,16 @@ const LoginContainer: React.FC = () => {
     const res = await DefaultService.login(login_req);
     OpenAPI.TOKEN = res.access_token;
     localStorage.setItem('token', res.access_token);
+
+    const user_info = await DefaultService.getCurrentUserInfo();
+    if (user_info.roles.includes(UserRole.DATA_SUBMITTER)) {
+      navigate('/dashboard/datasets');
+    } else if (user_info.roles.includes(UserRole.RESEARCHER)) {
+      navigate('/dashboard/researcher-portal');
+    } else {
+      navigate('/dashboard');
+    }
+
     return res;
   }
 
